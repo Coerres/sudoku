dabca883b4a4b858c5bda78339128410f2b0dec8
Carina Cordes <carina.cordes@ksb-sg.ch>
2024-07-20 00:21:09 +0200

    Load the Game

--- sudoku.js
+++ sudoku.js
@@ -27,3 +27,29 @@ var solution = [
     "675832941",
     "812945763"
 ]
+
+window.onload = function(){
+    setGame();
+}
+
+function setGame() {
+    // Digits 1-9
+    for(let i = 1; i<=9; i++) {
+        // <div id="1" class="number">1</div>
+        let number = document.createElement("div");
+        number.id = i;
+        number.innerText = i;
+        number.classList.add("number");
+        document.getElementById("digits").appendChild(number);
+    }
+
+    // Board 9x9
+    for(let r = 0; r < 9; r++){
+        for(let c = 0; c < 9; c++){
+            let tile = document.createElement("div");
+            tile.id = r.toString() + "-" + c.toString();
+            tile.classList.add("tile");
+            document.getElementById("board").append(tile);
+        }
+    }
+}
